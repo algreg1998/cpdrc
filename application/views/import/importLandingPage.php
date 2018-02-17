b241998403415895d6bb734108a24c4beddaf1c4
algreg1998 <algreg17@gmail.com>
2018-02-17 22:14:05 +0800

    added sql from capstone defense

--- application/views/import/importLandingPage.php
+++ application/views/import/importLandingPage.php
@@ -38,7 +38,7 @@
                             <select name="type" class="form-control" style="width: 150px;">
                                 <option value="inmate">Inmate Records</option>
                                 <option value="violation">Violation Records</option>
-                                <!-- <option value="cases">Cases Records</option> -->
+                                <!-- <option <var></var>lue="cases">Cases Records</option> -->
                             </select>
                             <input type="submit" class="btn btn-primary" name="importSubmit" id="importSubmit" value="Import">
                         </form>
