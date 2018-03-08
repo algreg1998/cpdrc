5ddab626cc55842383cc760fbcf502a6f5478939
Darrel Solis <darrelsolis.360@gmail.com>
2018-03-09 07:18:00 +0800

    login validation fixed

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -47,7 +47,7 @@ class Login extends User_Controller {
 					$this->session->set_flashdata('error_msg','User Deactivated please contact Administrator.');
 				}
 			}else{
-				$this->session->set_flashdata('error_msg','Username and Password does not match.');
+				$this->session->set_flashdata('error_msg','Username and password does not match.');
 			}
 
 			redirect(current_url_full());
